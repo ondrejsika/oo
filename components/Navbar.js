c4505fe51fa04234e7fc2aedafd0710014445b1f
Zuzana Jeschke <zuz@zuzjes.com>
2020-05-31 11:47:25 +0200

    fix navbar color and alignment

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,18 +3,31 @@ import { Navbar, Nav } from "react-bootstrap";
 
 export default () => (
   <>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar.Toggle
+        aria-controls="responsive-navbar-nav"
+        className="ml-auto"
+      />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Jak si chránit svá stáda?</Nav.Link>
-          <Nav.Link href="#pricing">
-            Příklady z praxe/Zkušenosti farmářů
+        <Nav className="ml-auto">
+          <Nav.Link href="#features">
+            <b>Jak si chránit svá stáda?</b>
           </Nav.Link>
-          <Nav.Link href="">Co dělat při napadení?</Nav.Link>
-          <Nav.Link href="">Podpora prevence</Nav.Link>
-          <Nav.Link href="">O webu</Nav.Link>
-          <Nav.Link href="">Kontakt</Nav.Link>
+          <Nav.Link href="#pricing">
+            <b>Příklady z praxe/Zkušenosti farmářů</b>
+          </Nav.Link>
+          <Nav.Link href="">
+            <b>Co dělat při napadení?</b>
+          </Nav.Link>
+          <Nav.Link href="">
+            <b>Podpora prevence</b>
+          </Nav.Link>
+          <Nav.Link href="">
+            <b>O webu</b>
+          </Nav.Link>
+          <Nav.Link href="">
+            <b>Kontakt</b>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
