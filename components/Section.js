132f209a0ac1bf914413faa710d3d094b27efbd2
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-31 12:33:06 +0200

    Section - add mt-5, mb-5

--- components/Section.js
+++ components/Section.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 export default (props) => (
-  <Container>
+  <Container className="mt-5 mb-5">
     <h2>{props.header}</h2>
     {props.children}
   </Container>
