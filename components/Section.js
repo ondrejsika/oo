9354832c1cbb22e988d20285254dbb01f3a19efe
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-31 11:25:52 +0200

    Section - add container

--- components/Section.js
+++ components/Section.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 export default (props) => (
-  <>
+  <Container>
     <h2>{props.header}</h2>
     {props.children}
-  </>
+  </Container>
 );
