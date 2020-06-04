3dfa149ba2f573dd726b506623580e1caa9d5911
Zuzana Jeschke <zuz@zuzjes.com>
2020-06-04 21:12:54 +0200

    fix header text and add new image

--- components/Header.js
+++ components/Header.js
@@ -3,23 +3,43 @@ import { Container } from "react-bootstrap";
 import styled from "styled-components";
 
 const H1 = styled.h1`
-  color: #575757;
-  padding: 1em 0 7em 0;
+  color: white;
+  /* margin-bottom: 0; */
+  display: inline;
   font-weight: bold;
+  font-size: 3em;
+  text-transform: uppercase;
+  background-color: grey;
 `;
-const Background = styled.div`
-  background-image: url("/static/images/header.jpeg");
+const TextBackground = styled.span`
+  background-color: grey;
+  padding: 5px;
+`;
+
+const BackgroundImg = styled.div`
+  background-image: url("/static/images/sheep.jpg");
   background-repeat: no-repeat;
   background-size: cover;
+  padding: 50px;
+  /* height: 200px; */
 `;
 export default (props) => (
-  <Background>
-    <Container>
-      <H1>
-        POMOCNÍK
-        <br /> pro chovatele hospodářských zvířat
-      </H1>
-      {props.children}
-    </Container>
-  </Background>
+  <>
+    <BackgroundImg>
+      {/* <Background> */}
+      <Container>
+        <H1>
+          <TextBackground>
+            &nbsp;POMOCNÍK&nbsp;
+            <br />
+            &nbsp;pro chovatele&nbsp;
+            <br />
+            &nbsp;hospodářských zvířat&nbsp;
+          </TextBackground>
+        </H1>
+        {props.children}
+      </Container>
+      {/* </Background> */}
+    </BackgroundImg>
+  </>
 );
