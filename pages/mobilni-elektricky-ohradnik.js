fc732b26d00ff059c98a588912880822cd4364d9
Ondrej Sika <ondrej@ondrejsika.com>
2020-06-04 20:32:43 +0200

    Move large articles to own pages

---
+++ pages/mobilni-elektricky-ohradnik.js
@@ -0,0 +1,109 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Section from "../components/Section";
+import Article from "../components/Article";
+
+export default () => (
+  <>
+    <Navbar />
+    <Section header="MOBILNÍ ELEKTRICKÝ OHRADNÍK">
+      <Article>
+        <p>
+          Elektrický ohradník je jedním z nejoblíbenějších a nejúčinnějších
+          ochranných opatření proti velkým šelmám.
+        </p>
+      </Article>
+      <Article header="Použití">
+        <p>
+          Lankový nebo síťový elektrický ohradník mají univerzální použití.
+          Logicky jsou vhodné především tam, kde se místo pastvy během roku mění
+          nebo je třeba v zájmu ochrany přírody zachovat průchodnou krajinu mimo
+          období pastvy. Údržba vegetace v okolí ohradníků je snazší, protože je
+          možné ohradníky snadno posunout nebo nadzvednout lanka. Síťové
+          ohradníky je vhodnější instalovat v málo členitých terénech, ale mohou
+          být méně stabilní na návětrných svazích a mají větší délku drátů, tedy
+          i vyšší odběr ve srovnání s lankovým ohradníkem. Lankové elektrické
+          ohradníky mají oproti síťovým výhodu, že zvířata s rohy se do něj
+          nezamotají.
+        </p>
+      </Article>
+      <Article header="Účinnost">
+        <p>
+          Správně instalovaný a udržovaný elektrický ohradník o minimální výšce
+          120 cm je jedním z nejúčinnějších ochranných opatření proti šelmám.
+          Samotný ohradník představuje fyzickou překážku a elektrický šok je pro
+          zvíře, které se plotu dotkne, nepříjemný. U lankových ohradníků je
+          třeba, aby nejnižší drát byl v minimální výšce 20-25 cm nad zemí, aby
+          šelmám zabránil podlezení. Jednou ze slabin ohradníku je, že zvířata
+          vyplašená vlky ho mohou sama protrhnout. Nejzásadnější je proto
+          ponechat zvířatům dostatek místa, aby se cítila v bezpečí před šelmami
+          zvenčí. Účinnost může být zvýšena přítomností pasteveckého psa uvnitř
+          ohrady.
+        </p>
+      </Article>
+      <Article header="Účinnost">
+        <p>
+          Elektrický ohradník vyžaduje středně vysokou vstupní investici; je
+          třeba obstarat sítě nebo lanka se sloupky, zdroj impulzů a zkoušečku
+          napětí. V lokalitách, kde není přístup elektřiny, je nutné používat
+          akumulátory, případně dobíjení s malými solárními panely, což zvyšuje
+          vstupní investici. Poměrná cena je tím nižší, čím větší je velikost
+          ohrazeného prostoru. Pokud je ohradník stále pod napětím, fyzický
+          kontakt se zvířaty (ať už s hospodářskými nebo divokými) je minimální
+          a lankový ohradník z plastových lanek má dlouhou životnost. Cena
+          pochopitelně závisí na typu a kvalitě použitých sloupků, výkonu zdroje
+          a přítomnosti akumulátoru či solárního panelu.
+        </p>
+      </Article>
+      <Article header="Požadavky a doporučení při instalaci">
+        <h4>Lankový ohradník</h4>
+        <ul>
+          <li>alespoň 5 vodičů</li>
+          <li>výška ohradníku 120-150 cm</li>
+          <li>nejnižší vodič 20-25 cm nad zemí</li>
+          <li>vzdálenost mezi vodiči 20-30 cm</li>
+          <li>
+            doporučujeme instalovat horní signální pásku bílé barvy alespoň 2 cm
+            širokou. Tato páska nemusí být nutně pod napětím, ale zvyšuje
+            viditelnost pro predátory a další divoce žijící zvířata.
+          </li>
+        </ul>
+        <h4>Síťový ohradník</h4>
+        <ul>
+          <li>
+            120 cm výšky nebo 90 cm s přídavnou vodivou páskou ve výšce 120 cm
+          </li>
+          <li>
+            síťový ohradník musí být dostatečně napnutý, aby v kontaktu se zemí
+            nebo vegetací nebyl druhý nejnižší vodorovný vodič, který je pod
+            napětím. V členitém terénu je použití síťových ohradníků
+            komplikovanější při lomu terénu.
+          </li>
+        </ul>
+        <h4>Všechny typy</h4>
+        <ul>
+          <li>napětí alespoň 4500 V</li>
+          <li>
+            ospodářská zvířata by měla mít uvnitř ohradníku dostatek bezpečného
+            prostoru (minimálně 2000 metrů čtverečních), aby se v případě pohybu
+            šelem v blízkosti ohradníku nevyplašila a ohradník v panice
+            neprotrhla
+          </li>
+          <li>
+            pro funkčnost opatření je zásadní, aby ohradník byl stále pod
+            napětím. Je potřeba pravidelně kontrolovat, zda se vodiče nedotýkají
+            vegetace a ohradník tak není uzemněný. Lze využít zkoušečky,
+            signální světla či SMS hlásiče
+          </li>
+          <li>
+            Během suchých období na písčitých půdách může být problém v tom, že
+            zvířata, která se dotýkají sítí/lanek nejsou uzemněná, a tudíž jimi
+            neprojde elektrický výboj. Řešením je v ohradníku střídat vodiče ze
+            zdroje (+) s vodiči napojenými na dobře uzemněnou zemnící tyč (-). K
+            výboji pak dojde, když se zvíře dotkne obou vodičů současně.
+          </li>
+        </ul>
+      </Article>
+    </Section>
+  </>
+);
---
+++ pages/pevny-ohradnik.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Section from "../components/Section";
+import Article from "../components/Article";
+
+export default () => (
+  <>
+    <Navbar />
+    <Section header="PEVNÝ OHRADNÍK">
+      <p>
+        Pevné ohrady se běžně používají k vymezení pastvin, kde jsou zvířata po
+        většinu pastevní sezóny.
+      </p>
+      <Article header="Použití">
+        <p>
+          Pevné ohrady se běžně používají k vymezení pastvin, kde jsou zvířata
+          po většinu pastevní sezóny. Tyto ohrady však většinou nejsou postavené
+          tak, aby stádo uchránily před útokem predátorů. Aby zvířata ohradou
+          neprolezla, je nutné ohradu zajistit proti podhrabání:
+        </p>
+        <ul>
+          <li>
+            Z vnější strany ohrady instalovat vodič na (20 cm) předsazených
+            izolátorech ve výšce 20-25 cm nad zemí.
+          </li>
+        </ul>
+        <p>nebo</p>
+        <li>
+          Mechanicky zapustit drátěný plot min. 50 cm pod zem nebo před ohradu
+          položit a pevně zakotvit pletivo navazující na plot v šířce min. 100
+          cm před ohradníkem .
+        </li>
+        <p>
+          Alternativou k mechanické ochraně proti podhrabání je využití
+          pasteveckých psů v pevné ohradě.{" "}
+        </p>
+        <p>
+          Minimální výška pevné elektrifikované ohrady by stejně jako u
+          mobilních ohradníků měla být 120 cm, lze ji navýšit o přídavný drát až
+          do výšky 150 cm. Samotná ohrada může mít různé podoby: uzlíkové nebo
+          vysokopevnostní pletivo na akátových kůlech nebo vypnutné dráty o
+          průměru 1,5-2,5 mm pod napětím s rozestupy 20-30 cm, s nejnižším
+          vodičem 20 cm nad zemí.
+        </p>
+      </Article>
+      <Article header="Účinnost">
+        <p>
+          Správně udržovaná pevná ohrada s elektrickými vodiči, bránící
+          podhrabání, má podobnou účinnost jako mobilní elektrické ohradníky.
+          Výhodou je, že zvířata vyplašená vlky pevnou ohradu zpravidla
+          neprotrhnou. Je však třeba důsledně kontrolovat napětí v ohradníku a
+          všímat si, zda jsou proti podhrabání chráněna i slabá místa na ohradě,
+          tedy zejména brány.
+        </p>
+      </Article>
+      <Article header="Cena">
+        <p>
+          Pevná ohrada je velkou vstupní investicí. Pokud však chovatel již
+          pevnou ohradu má nebo ji plánuje postavit či obnovit kvůli vymezení
+          pastviny pro hospodářská zvířata, nejsou přídavné prvky zajišťující
+          ochranu proti vniknutí predátorů většinou významným navýšením - jde
+          zejména o zvýšení počtu vodičů tak, aby ohrada splňovala minimální,
+          výše popsané parametry. Náklady na následnou údržbu zahrnují zejména
+          pravidelné podsekávání, aby nejnižší vodiče nezarůstaly trávou.
+        </p>
+      </Article>
+      <Article header="Požadavky a doporučení při instalaci">
+        <ul>
+          <li>Alespoň 5 vodičů.</li>
+          <li>Výška ohradníku 120-150 cm.</li>
+          <li>Nejnižší vodič 20-25 cm nad zemí.</li>
+          <li>Vzdálenost mezi vodiči 20-30 cm.</li>
+          <li>Napětí alespoň 4500 V.</li>
+          <li>
+            Pro funkčnost opatření je zásadní, aby vodiče byly stále pod
+            napětím. Je potřeba pravidelně kontrolovat, zda se vodiče nedotýkají
+            vegetace a ohradník tak není uzemněný. Lze využít zkoušečky,
+            signální světla či SMS hlásiče.
+          </li>
+          <li>
+            Pokud nechcete nebo nemůžete ohradník pravidelně podsekávat, lze pod
+            nejnižším lankem bránícím šelmám podhrabání instalovat fólií bránící
+            prorůstání trávy.
+          </li>
+          <li>
+            K podsekávání rozsáhlých ohrad je možné použít speciální sekačky na
+            traktor, které zvládnou podsekat i nejnižší vodič a objet kůl.
+          </li>
+        </ul>
+      </Article>
+    </Section>
+  </>
+);
