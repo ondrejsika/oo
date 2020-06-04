ebebd10b2c8c0882745bd1d18c0e9213f16fe485
Ondrej Sika <ondrej@ondrejsika.com>
2020-06-04 22:42:09 +0200

    Button - add support for target (blank)

--- components/Button.js
+++ components/Button.js
@@ -6,14 +6,16 @@ const classes = "btn btn-large btn-outline-secondary";
 export default (props) => {
   if (props.href.startsWith("http") || props.href.startsWith("mailto"))
     return (
-      <a className={classes} href={props.href} classes>
+      <a target={props.target} className={classes} href={props.href} classes>
         {props.children}
       </a>
     );
   return (
     <>
       <Link href={props.href}>
-        <a className={classes}>{props.children}</a>
+        <a target={props.target} className={classes}>
+          {props.children}
+        </a>
       </Link>
     </>
   );
