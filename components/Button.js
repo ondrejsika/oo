4ba81d414382c022282a36862afbd540b4d1716e
Ondrej Sika <ondrej@ondrejsika.com>
2020-06-04 22:37:09 +0200

    Button - use simple a href for external & email links

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Link from "next/link";
 
-export default (props) => (
-  <>
-    <Link href={props.href}>
-      <a className="btn btn-large btn-outline-primary">{props.children}</a>
-    </Link>
-  </>
-);
+const classes = "btn btn-large btn-outline-primary";
+
+export default (props) => {
+  if (props.href.startsWith("http") || props.href.startsWith("mailto"))
+    return (
+      <a className={classes} href={props.href} classes>
+        {props.children}
+      </a>
+    );
+  return (
+    <>
+      <Link href={props.href}>
+        <a className={classes}>{props.children}</a>
+      </Link>
+    </>
+  );
+};
